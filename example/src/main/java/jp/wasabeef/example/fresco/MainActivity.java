86a4f4deb6c95cee838cfcb5113cb49a0460e908
suiyueming <sui_yueming@163.com>
2016-05-25 18:23:01 +0800

    dsfvdsvf

--- example/src/main/java/jp/wasabeef/example/fresco/MainActivity.java
+++ example/src/main/java/jp/wasabeef/example/fresco/MainActivity.java
@@ -16,42 +16,86 @@ package jp.wasabeef.example.fresco;
  * limitations under the License.
  */
 
+import android.content.Context;
+import android.graphics.Color;
+import android.graphics.PointF;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import com.facebook.drawee.backends.pipeline.Fresco;
+import com.facebook.drawee.backends.pipeline.PipelineDraweeController;
+import com.facebook.drawee.drawable.ScalingUtils;
+import com.facebook.imagepipeline.request.ImageRequest;
+import com.facebook.imagepipeline.request.ImageRequestBuilder;
+import com.facebook.imagepipeline.request.Postprocessor;
+
+import net.grobas.view.PolygonImageView;
+
 import java.util.ArrayList;
 import java.util.List;
 import jp.wasabeef.example.fresco.MainAdapter.Type;
+import jp.wasabeef.fresco.processors.BlurPostprocessor;
+import jp.wasabeef.fresco.processors.ColorFilterPostprocessor;
+import jp.wasabeef.fresco.processors.GrayscalePostprocessor;
+import jp.wasabeef.fresco.processors.MaskPostprocessor;
+import jp.wasabeef.fresco.processors.gpu.BrightnessFilterPostprocessor;
+import jp.wasabeef.fresco.processors.gpu.ContrastFilterPostprocessor;
+import jp.wasabeef.fresco.processors.gpu.InvertFilterPostprocessor;
+import jp.wasabeef.fresco.processors.gpu.KuawaharaFilterPostprocessor;
+import jp.wasabeef.fresco.processors.gpu.PixelationFilterPostprocessor;
+import jp.wasabeef.fresco.processors.gpu.SepiaFilterPostprocessor;
+import jp.wasabeef.fresco.processors.gpu.SketchFilterPostprocessor;
+import jp.wasabeef.fresco.processors.gpu.SwirlFilterPostprocessor;
+import jp.wasabeef.fresco.processors.gpu.ToonFilterPostprocessor;
+import jp.wasabeef.fresco.processors.gpu.VignetteFilterPostprocessor;
 
 public class MainActivity extends AppCompatActivity {
 
+
+
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    Fresco.initialize(this);
-    setContentView(R.layout.activity_main);
+      Fresco.initialize(this);
+      setContentView(R.layout.test_layout);
+      PolygonImageView simpleDraweeView = (PolygonImageView) findViewById(R.id.test_image);
+      simpleDraweeView.setImageResource(R.drawable.default_user_image);
 
-    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
-    recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+//      simpleDraweeView.getHierarchy().setActualImageScaleType(ScalingUtils.ScaleType.FIT_CENTER);
+//
+//      ImageRequest request = ImageRequestBuilder.newBuilderWithResourceId(R.drawable.demo)
+//              .build();
+//
+//      PipelineDraweeController controller =
+//              (PipelineDraweeController) Fresco.newDraweeControllerBuilder()
+//                      .setImageRequest(request)
+//                      .setOldController(simpleDraweeView.getController())
+//                      .build();
+//      simpleDraweeView.setController(controller);
 
-    List<Type> dataSet = new ArrayList<>();
-    dataSet.add(Type.Blur);
-    dataSet.add(Type.Grayscale);
-    dataSet.add(Type.ColorFilter);
-    dataSet.add(Type.Mask);
-    dataSet.add(Type.NinePatchMask);
-    dataSet.add(Type.Pixel);
-    dataSet.add(Type.Vignette);
-    dataSet.add(Type.Kuawahara);
-    dataSet.add(Type.Brightness);
-    dataSet.add(Type.Swirl);
-    dataSet.add(Type.Sketch);
-    dataSet.add(Type.Invert);
-    dataSet.add(Type.Contrast);
-    dataSet.add(Type.Sepia);
-    dataSet.add(Type.Toon);
 
-    recyclerView.setAdapter(new MainAdapter(this, dataSet));
+//    setContentView(R.layout.activity_main);
+//
+//    RecyclerView recyclerView = (RecyclerView) findViewById(R.id.list);
+//    recyclerView.setLayoutManager(new GridLayoutManager(this, 2));
+//
+//    List<Type> dataSet = new ArrayList<>();
+//    dataSet.add(Type.Blur);
+//    dataSet.add(Type.Grayscale);
+//    dataSet.add(Type.ColorFilter);
+//    dataSet.add(Type.Mask);
+//    dataSet.add(Type.NinePatchMask);
+//    dataSet.add(Type.Pixel);
+//    dataSet.add(Type.Vignette);
+//    dataSet.add(Type.Kuawahara);
+//    dataSet.add(Type.Brightness);
+//    dataSet.add(Type.Swirl);
+//    dataSet.add(Type.Sketch);
+//    dataSet.add(Type.Invert);
+//    dataSet.add(Type.Contrast);
+//    dataSet.add(Type.Sepia);
+//    dataSet.add(Type.Toon);
+//
+//    recyclerView.setAdapter(new MainAdapter(this, dataSet));
   }
 }
--- example/src/main/java/jp/wasabeef/example/fresco/MaskImage.java
+++ example/src/main/java/jp/wasabeef/example/fresco/MaskImage.java
@@ -10,48 +10,113 @@ import android.graphics.Paint;
 import android.graphics.PorterDuff;
 import android.graphics.PorterDuffXfermode;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.View;
+import android.view.ViewGroup;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 
 import com.facebook.drawee.view.DraweeView;
 
-public class MaskImage extends DraweeView{
-    int mImageSource=0;
-    int mMaskSource=0;
-    RuntimeException mException;
+public class MaskImage extends ViewGroup{
+    private static final String TAG = "MaskImage";
+    private float borderWidth;
+
+    int SimpleDraweeViewWidth;
+    int SimpleDraweeViewHeight;
+
 
     public MaskImage(Context context, AttributeSet attrs) {
         super(context, attrs);
-        TypedArray a = getContext().obtainStyledAttributes(attrs, R.styleable.MaskImage, 0, 0);
-        mImageSource = a.getResourceId(R.styleable.MaskImage_image, 0);
-        mMaskSource = a.getResourceId(R.styleable.MaskImage_mask, 0);
+    }
 
-        if (mImageSource == 0 || mMaskSource == 0) {
-            mException = new IllegalArgumentException(a.getPositionDescription() +
-                    ": The content attribute is required and must refer to a valid image.");
+
+    @Override
+    public ViewGroup.LayoutParams generateLayoutParams(AttributeSet attrs) {
+        return new ViewGroup.MarginLayoutParams(getContext(), attrs);
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateLayoutParams(ViewGroup.LayoutParams p) {
+        return new ViewGroup.MarginLayoutParams(p);
+    }
+
+    @Override
+    protected ViewGroup.LayoutParams generateDefaultLayoutParams() {
+        return new ViewGroup.MarginLayoutParams(ViewGroup.LayoutParams.MATCH_PARENT,
+                ViewGroup.LayoutParams.MATCH_PARENT);
+    }
+
+    @Override
+    protected void onMeasure(int widthMeasureSpec, int heightMeasureSpec) {
+
+
+        super.onMeasure(widthMeasureSpec, heightMeasureSpec);
+
+        int sizeWidth = MeasureSpec.getSize(widthMeasureSpec);
+        int sizeHeight = MeasureSpec.getSize(heightMeasureSpec);
+        int modeWidth = MeasureSpec.getMode(widthMeasureSpec);
+        int modeHeight = MeasureSpec.getMode(heightMeasureSpec);
+
+        //如果是warp_content情况下，记录宽和高
+        int width = 0;
+        int height = 0;
+
+        int childCount = this.getChildCount();
+        for (int i=0; i<childCount; i++) {
+            View child = getChildAt(i);
+            measureChild(child, widthMeasureSpec, heightMeasureSpec);
+            ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) child
+                    .getLayoutParams();
+            // 当前子空间实际占据的宽度
+            int childWidth = child.getMeasuredWidth() + lp.leftMargin
+                    + lp.rightMargin;
+            // 当前子空间实际占据的高度
+            int childHeight = child.getMeasuredHeight() + lp.topMargin
+                    + lp.bottomMargin;
+            if(child.getClass() == com.facebook.drawee.view.SimpleDraweeView.class) {
+                SimpleDraweeViewWidth = childWidth;
+                SimpleDraweeViewHeight = childHeight;
+            }
+            width += childWidth;
+            height += childHeight;
+
         }
 
-        if (mException != null)
-            throw mException;
-        /**
-         * 主要代码实现
-         */
-        //获取图片的资源文件
-        Bitmap original = BitmapFactory.decodeResource(getResources(), mImageSource);
-        //获取遮罩层图片
-        Bitmap mask = BitmapFactory.decodeResource(getResources(), mMaskSource);
-        Bitmap result = Bitmap.createBitmap(mask.getWidth(), mask.getHeight(), Config.ARGB_8888);
-        //将遮罩层的图片放到画布中
-        Canvas mCanvas = new Canvas(result);
-        Paint paint = new Paint(Paint.ANTI_ALIAS_FLAG);
-        paint.setXfermode(new PorterDuffXfermode(PorterDuff.Mode.DST_IN));//叠加重复的部分，显示下面的
-        mCanvas.drawBitmap(original, 0, 0, null);
-        mCanvas.drawBitmap(mask, 0, 0, paint);
+        setMeasuredDimension((modeWidth == MeasureSpec.EXACTLY) ? sizeWidth
+                : width, (modeHeight == MeasureSpec.EXACTLY) ? sizeHeight
+                : height);
 
-        paint.setXfermode(null);
-        setImageBitmap(result);
-        setScaleType(ScaleType.CENTER);
+    }
 
-        a.recycle();
+    @Override
+    protected void onLayout(boolean changed, int l, int t, int r, int b) {
+        int childCount = this.getChildCount();
+        Log.e(TAG, "l = " + l + " r = " + r + " t = " + t + " b = " + b);
+
+        for (int i =0; i<childCount; i++) {
+            View child = getChildAt(i);
+            ViewGroup.MarginLayoutParams lp = (ViewGroup.MarginLayoutParams) child
+                    .getLayoutParams();
+            int childWidth = child.getMeasuredWidth();
+            int childHeight = child.getMeasuredHeight();
+            Log.e(TAG, child.getClass().toString());
+            if(child.getClass() == net.grobas.view.PolygonImageView.class) {
+                child.layout((l + lp.leftMargin),
+                        (t + lp.topMargin),
+                        (l + lp.leftMargin + childWidth),
+                        (t + lp.topMargin + childHeight));
+            } else if (child.getClass() == LinearLayout.class){
+                Log.e(TAG, "LinearLayout");
+
+            }
+
+        }
+    }
+
+    @Override
+    protected void onDraw(Canvas canvas) {
+        super.onDraw(canvas);
     }
 }
 
